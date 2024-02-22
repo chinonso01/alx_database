478609bdaf6bd334d6890b8e103c4840f824de86
chinonso01 <irobindachinonso>
2024-02-22 20:11:59 +0100

    commit

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,3 +1,11 @@
 -- Active: 1708628626646@@localhost@3306@good
 --list all database in mysql server
-SHOW DATABASES;
+#!/bin/bash
+
+# Prompt for MySQL credentials
+read -p "MySQL Username: " username
+read -s -p "MySQL Password: " password
+echo
+
+# MySQL command to list databases
+mysql -u"$username" -p"$password" -e "SHOW DATABASES;"
