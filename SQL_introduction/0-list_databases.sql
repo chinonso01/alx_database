ab9ba62de997a8f004229337b708689ef66571ee
chinonso01 <irobindachinonso>
2024-02-26 18:17:31 +0100

    commit

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,3 +1,13 @@
--- Active: 1708628626646@@localhost@3306@good
+-- Active: 1708628626646@@localhost@3306@testdb
 --list all database in mysql server
 SHOW DATABASES;
+CREATE TABLE employees(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50),
+    dob DATETIME,
+    email VARCHAR(70),
+    phone INTEGER(11)
+);
+INSERT INTO employees (id, name, dob, email, phone)
+VALUES(1, 'Madhur', '1989-11-20 11:55:00', 'madhur@gmail.com', 0904489562);
+SELECT * FROM employees;
