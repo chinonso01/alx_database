3c34171ce34026171bf8fbebee739b5ab4f26bf2
chinonso01 <irobindachinonso>
2024-02-27 17:18:38 +0100

    commit

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,24 @@
--- Active: 1708628626646@@localhost@3306@testd
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'first_table'
-AND TABLE_SCHEMA = 'hbtn_0c_0';
+#!/bin/bash
+
+# Check if the correct number of arguments are passed
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <database_name>"
+    exit 1
+fi
+
+# Assign the argument to a variable
+database_name=$1
+
+# Create a temporary SQL file with the queries
+temp_sql_file=$(mktemp /tmp/mysql_script.XXXXXX)
+
+echo "USE information_schema;" >> "$temp_sql_file"
+echo "SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA, COLUMN_COMMENT" >> "$temp_sql_file"
+echo "FROM COLUMNS" >> "$temp_sql_file"
+echo "WHERE TABLE_SCHEMA = '$database_name' AND TABLE_NAME = 'first_table';" >> "$temp_sql_file"
+
+# Execute the SQL file using the mysql command
+mysql -h your_mysql_host -u your_mysql_user -p < "$temp_sql_file"
+
+# Remove the temporary SQL file
+rm "$temp_sql_file"
