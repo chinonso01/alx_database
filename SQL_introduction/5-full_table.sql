987a54e5af15b3ac640cdbfe95137fdeb8d6ed4a
chinonso01 <irobindachinonso>
2024-02-27 19:23:39 +0100

    commit

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,9 +1,2 @@
 -- Active: 1708628626646@@localhost@3306@testd
-CREATE TABLE IF NOT EXISTS 'first_table' (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(128) DEFAULT NULL,
-    c CHAR(1) DEFAULT NULL,
-    created_at DATE DEFAULT NULL,
-    PRIMARY KEY(id)
-)
-DESC first_table;
+DESC hbtn_0c_0.first_table;
