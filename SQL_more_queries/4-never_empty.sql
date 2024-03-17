72644c60c6a5c987489e75215865bb7bc447d846
chinonso01 <irobindachinonso>
2024-03-17 12:38:46 +0100

    com

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,7 +1,21 @@
-Write a script that creates the table id_not_null on your MySQL server.
+#!/bin/bash
 
-id_not_null description:
-id INT with the default value 1
-name VARCHAR(256)
-The database name will be passed as an argument of the mysql command
-If the table id_not_null already exists, your script should not fail
+# Script to create the table id_not_null in a MySQL database
+
+# Check if the database name is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Error: Database name is not provided."
+    exit 1
+fi
+
+# Assign the database name provided as an argument
+database_name="$1"
+
+# SQL query to create the id_not_null table
+create_table_query="CREATE TABLE IF NOT EXISTS $database_name.id_not_null (
+    id INT DEFAULT 1,
+    name VARCHAR(256)
+);"
+
+# Execute the SQL query using the mysql command
+mysql -u username -p -e "$create_table_query"
