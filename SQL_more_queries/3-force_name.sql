716394a6675819da70da47ddba51c48c0031c097
chinonso01 <irobindachinonso>
2024-03-17 12:28:19 +0100

    commit

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script to create the table force_name in a MySQL database
+# Script to create the force_name table on MySQL server
 
 # Check if the database name is provided as an argument
 if [ $# -eq 0 ]; then
@@ -12,12 +12,10 @@ fi
 database_name="$1"
 
 # SQL query to create the force_name table
-create_table_query="^-- Create the force_name table if it doesn't exist
-CREATE TABLE IF NOT EXISTS $database_name.force_name (
-    id INT, -- ID column
-    name VARCHAR(256) NOT NULL -- Name column can't be null
+create_table_query="CREATE TABLE IF NOT EXISTS $database_name.force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
 );"
 
 # Execute the SQL query using the mysql command
-mysql -u username -p -e "$create_table_query"
-
+echo "$create_table_query" | mysql -hlocalhost -uroot -p "$database_name"
