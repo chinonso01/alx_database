ee7aa7797c3054e77c8ed92cb32aa5d674b97b72
chinonso01 <irobindachinonso>
2024-03-17 12:26:41 +0100

    com

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -12,10 +12,12 @@ fi
 database_name="$1"
 
 # SQL query to create the force_name table
-create_table_query="CREATE TABLE IF NOT EXISTS $database_name.force_name (
-    id INT,
-    name VARCHAR(256) NOT NULL
+create_table_query="^-- Create the force_name table if it doesn't exist
+CREATE TABLE IF NOT EXISTS $database_name.force_name (
+    id INT, -- ID column
+    name VARCHAR(256) NOT NULL -- Name column can't be null
 );"
 
 # Execute the SQL query using the mysql command
 mysql -u username -p -e "$create_table_query"
+
