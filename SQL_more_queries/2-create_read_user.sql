b84d2e2b7dae5d3db066d549b8b2b1121e72c643
chinonso01 <irobindachinonso>
2024-03-17 12:12:18 +0100

    commit

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,10 +1,15 @@
--- This script creates the database hbtn_0d_2 and the user user_0d_2.
+-- This script creates the database hbtn_0d_2 and the user user_0d_2 if they don't exist.
 
 -- Creating the database hbtn_0d_2 if it doesn't already exist.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
--- Creating or updating the user user_0d_2 and setting the password.
-CREATE OR REPLACE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- Checking if the user user_0d_2 exists.
+SELECT COUNT(*)
+FROM mysql.user
+WHERE user = 'user_0d_2';
+
+-- Creating the user user_0d_2 only if it doesn't already exist.
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Granting SELECT privilege on the database hbtn_0d_2 to the user user_0d_2.
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
