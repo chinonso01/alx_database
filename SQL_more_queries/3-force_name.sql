6efae237ebc8428f02a803032e8ca451f61d1ee1
chinonso01 <irobindachinonso>
2024-03-17 12:23:54 +0100

    commit

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Script to create the table force_name in a MySQL database
+
+# Check if the database name is provided as an argument
+if [ $# -eq 0 ]; then
+    echo "Error: Database name is not provided."
+    exit 1
+fi
+
+# Assign the database name provided as an argument
+database_name="$1"
+
+# SQL query to create the force_name table
+create_table_query="CREATE TABLE IF NOT EXISTS $database_name.force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);"
+
+# Execute the SQL query using the mysql command
+mysql -u username -p -e "$create_table_query"
